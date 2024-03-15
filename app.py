e5f024d60b59a6ebabba263f33a09cbf7a72661f
Monika-kompal97 <163507686+Monika-kompal97@users.noreply.github.com>
2024-03-15 15:43:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ import firebase_admin
 from firebase_admin import credentials, auth, db,firestore,storage
 import re
 
-cred = credentials.Certificate("C:/Users/Admin/OneDrive/Desktop/Majorproject (5)/Majorproject/majorproject-550be-firebase-adminsdk-wzfdz-4b636ba1b9.json")
+cred = credentials.Certificate("majorproject-550be-firebase-adminsdk-wzfdz-4b636ba1b9.json")
 firebase_admin.initialize_app(cred,{
     'storageBucket':'majorproject-550be.appspot.com'
 })
 db=firestore.client()
 bucket = storage.bucket()
-app = Flask(__name__,template_folder="C:/Users/Admin/OneDrive/Desktop/Majorproject (5)/Majorproject/templates")
+app = Flask(__name__,template_folder="templates")
 app.secret_key = 'Monu@143'
 # camera = cv2.VideoCapture(0)  # Initialize webcam
 
